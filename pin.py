165173b814c05ca6a2d68256b38c52ce7ffee169
6eom9eun <104510730+6eom9eun@users.noreply.github.com>
2022-09-26 20:17:09 +0900

    Add files via upload

--- pin.py
+++ pin.py
@@ -2,10 +2,9 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
 import os
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
 PATH = os.path.join('C:/Users/6eom9eun/Desktop/pin')
 
@@ -34,8 +33,8 @@ print("Total training images:", total_train)
 print("Total validation images:", total_val)
 
 
-batch_size = 100
-epochs = 8
+batch_size = 20
+epochs = 6
 IMG_HEIGHT = 150
 IMG_WIDTH = 150
 
@@ -59,20 +58,6 @@ sample_training_images, _ = next(train_data_gen)
 
 #plotImages(sample_training_images[:5])
 
-batch_size = 128
-epochs = 8
-IMG_HEIGHT = 150
-IMG_WIDTH = 150
-
-train_image_generator = ImageDataGenerator(rescale = 1./255)
-validation_image_generator = ImageDataGenerator(rescale=1./255)
-image_gen = ImageDataGenerator(rescale=1./255, horizontal_flip=True)
-
-train_data_gen = train_image_generator.flow_from_directory(batch_size=batch_size, directory=train_dir, shuffle=True, target_size=(IMG_HEIGHT, IMG_WIDTH), class_mode='binary')
-val_data_gen = validation_image_generator.flow_from_directory(batch_size=batch_size, directory=validation_dir, target_size=(IMG_HEIGHT, IMG_WIDTH), class_mode='binary')
-
-sample_training_images, _ = next(train_data_gen)
-
 model = Sequential([
     Conv2D(16, 3, padding = 'same', activation = 'relu', input_shape = (IMG_HEIGHT, IMG_WIDTH, 3)),
     MaxPooling2D(),
@@ -91,18 +76,19 @@ model.compile(optimizer='adam',
 
 model.summary()
 
-hist = model.fit_generator(
+hist = model.fit(
     train_data_gen,
-    steps_per_epoch=50,
+    steps_per_epoch=len(train_dir),
     epochs=epochs,
     validation_data=val_data_gen,
-    validation_steps=total_val)
+    validation_steps=len(validation_dir)
+)
 
 acc = hist.history['accuracy']
-val_acc=hist.history['val_accuracy']
+val_acc = hist.history['val_accuracy']
 
-loss=hist.history['loss']
-val_loss=hist.history['val_loss']
+loss = hist.history['loss']
+val_loss = hist.history['val_loss']
 
 epochs_range = range(epochs)
 
@@ -115,7 +101,7 @@ plt.title('Training and Validation Accuracy')
 
 plt.subplot(1, 2, 2)
 plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_acc, label='Validation Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
 plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
 plt.show()
